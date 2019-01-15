7963ad202e670e08282d8e297bf9253291015005
zhoushan <18230343435@163.com>
2019-01-15 21:20:01 +0800

    数据化搜索关键词

---
+++ baidu.py
@@ -0,0 +1,11 @@
+from selenium import webdriver
+lists = ["python","学习课程","飞机票","skittles"]
+for i in lists:
+    b = webdriver.Firefox()
+    b.get("http://www.baidu.com")
+    b.find_element_by_id("kw").clear()
+    b.find_element_by_id("kw").send_keys(i)
+    b.find_element_by_id("su").click()
+    
+    b.quit()
+    
